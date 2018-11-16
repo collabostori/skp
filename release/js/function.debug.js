82d1106a559dac96f426824aa8b4d0c6480e07c8
ministori <ministori@gmail.com>
2018-11-16 17:16:03 +0900

    1차 완료

--- release/js/function.debug.js
+++ release/js/function.debug.js
@@ -23,3 +23,60 @@ $(function(){
 
 
 
+
+$(function(){
+
+  /**
+   * gnb PC
+   */
+
+  /**
+   * gnb Mobile
+   */
+
+  $('.btn-mobile-gnb').data('open', 'false').on('click', function(){
+
+    if( $(this).data('open') === 'false' ){
+      $('html').addClass('gnb-open');
+      $('body').addClass('gnb-open');
+      $('.header').addClass('open');
+      $(this).addClass('open');
+      $(this).data('open', 'true');
+    } else {
+      $('html').removeClass('gnb-open');
+      $('body').removeClass('gnb-open');
+      $('.header').removeClass('open');
+      $(this).removeClass('open');
+      $(this).data('open', 'false');
+    }
+
+  });
+
+  $('.gnb-depth1-link').data('open', 'false').on('click', function(){
+
+    if( $(this).data('open') === 'false' ){
+      $('.gnb-depth1-link').removeClass('open').next().removeClass('open');
+      $(this).addClass('open').next().addClass('open');
+
+      $('.gnb-depth1-link').data('open', 'false');
+      $(this).data('open', 'true');
+    } else {
+      $(this).removeClass('open').next().removeClass('open');
+      $(this).data('open', 'false')
+    }
+
+  });
+
+  $('.tnb-current').data('open', 'false').on('click', function(){
+
+    if( $(this).data('open') === 'false' ){
+      $(this).addClass('open').next().addClass('open');
+      $(this).data('open', 'true');
+    } else {
+      $(this).removeClass('open').next().removeClass('open');
+      $(this).data('open', 'false');
+    }
+
+  });
+
+});
